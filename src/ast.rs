e823c80874f01e71e02de53a719e56b5a9c010e8
Liam Warfield <liam.warfield@gmail.com>
2020-05-03 12:55:41 -0600

    I added exporting to the AST graph, Other things will not compile, so I don't know if my stuff compiles

--- src/ast.rs
+++ src/ast.rs
@@ -1,4 +1,9 @@
-use petgraph::Graph
+use petgraph::dot::{Config, Dot};
+use petgraph::Graph;
+use std::path::Path;
+use std::fmt;
+use std::fs::File;
+use std::io::Write;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum AstKind {
@@ -31,7 +36,35 @@ pub enum AstKind {
     Bool(bool),
 }
 
+impl fmt::Display for AstKind { 
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            AstKind::Float(fl) => write!(f, "{}", fl),
+            AstKind::int(i) => write!(f, "{}", i),
+            AstKind::Bool(b) => write!(f, "{}", b),
+            AstKind::Eq => write!(f, "=="),
+            AstKind::Leq => write!(f, "<="),
+            AstKind::Geq => write!(f, ">="),
+            AstKind::Gt => write!(f, ">"),
+            AstKind::Lt => write!(f, "<"),
+            _ => write!(f, "{:?}", self),
+        }
+        // or, alternatively:
+        // fmt::Debug::fmt(self, f)
+    }
+}
+
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct AstGraph {
     pub graph: Graph<AstKind, usize>,
+}
+
+impl AstGraph {
+    //This writes out a .dot file to a path
+    pub fn export_graph(&self, file_path: impl AsRef<Path>) {
+        let mut f = File::create(file_path).unwrap();
+        let output = format!("{}", Dot::with_config(&self.graph, &[Config::EdgeNoLabel]));
+        f.write_all(&output.as_bytes())
+            .expect("could not write file");
+    }
 }
