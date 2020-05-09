4f42594d796d1b773d1970766f5f2292616b2846
Fisher Darling <fdarlingco@gmail.com>
2020-05-08 19:05:57 -0600

    whattt

--- src/symbol_table.rs
+++ src/symbol_table.rs
@@ -537,8 +537,12 @@ impl SymbolVisitor {
 
                             println!("{}", h.show_output());
                             self.errored = true;
-                        } else if !symbol.const_ {
-                            symbol.initialized.set(true);
+                        } else {
+                            if symbol.const_ && !symbol.initialized.get() {
+                                symbol.initialized.set(true);
+                            } else {
+                                symbol.initialized.set(true);
+                            }
                         }
                     }
                 }
